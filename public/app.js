2f6716054815562f4a96a6bfb37e131b10d8e4b1
Deven Davies <Deven_the_Impeccable@Devens-MacBook-Pro.local>
2015-11-19 17:31:44 -0500

    latest push, end of school day, 5:31

--- public/app.js
+++ public/app.js
@@ -1,22 +1,74 @@
 $(document).ready(function() {
   $('.artist-link').click(function(event) {
     // event.preventDefault();
-    console.log('working');
 
+    // Get and print all artists to screen
     $.get('/artists').done(function(data) {
       var source = $("#artist-template").html();
       var template = Handlebars.compile(source)
       $('body').append(template({artists: data}));
     })
-
   });
-});
+
+    $('.new-artist').click(function(event) {
+    // Get New Artist Parameters
+    var name        = $('#input-name').val();
+    var picture     = $('#input-picture').val();
+    var nationality = $('#input-nationality').val();
+    var birthYear   = $('#input-birthYear').val();
+    var description = $('#input-description').val();
+    var created_at  = Date.now;
+
+    // Create New Artist Based On Params
+    $.ajax({
+      type: "POST",
+      url:  "/artists",
+      data: {
+        name:        name,
+        img_url:     picture,
+        nationality: nationality,
+        birthYear:   birthYear,
+        description: description,
+        created_at:  created_at,
+        // PAINTINGS?
+      }
+    }).done(function(createdArtist) {
+      console.log(createdArtist);
+    });
+  }
+
+  // Get all art
+  $('.paintings-link').click(function(event) {
+    // event.preventDefault();
+
+    // Get and print all paintings to screen
+    $.get('/paintings').done(function(data) {
+      var source = $("#painting-template").html();
+      var template = Handlebars.compile(source)
+      $('body').append(template({paintings: data}));
+    })
+  });
+
+  // // Get New Artist Parameters
+  // var title       = ;
+  // var image       = ;
+  // var year_made   = ;
+  // var created_at  = Date.now;
+  //
+  // // Create New Artist Based On Params
+  // $.ajax({
+  //   type: "POST",
+  //   url:  "/paintings",
+  //   data: {
+  //     title: title,
+  //     img_url: image,
+  //     year_made: year,
+  //     created_at: created_at,
+  //     // Artist?
+  //   }
+  // }).done(function(createdPainting) {
+  //   console.log(createdPainting);
+  // });
 
 
-$.ajax({
-  type: "POST",
-  url: "/artists",
-  data: {name: "nicole", img_url: "lol", nationality: "lol"}
-}).done(function(createdArtist) {
-  console.log(createdArtist);
 });
