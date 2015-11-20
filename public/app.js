7b351fdb3e132c28f96500c95a26a1e4ac7a2046
napeters <nicole.asha.peters@gmail.com>
2015-11-19 22:50:52 -0500

    painting form

--- public/app.js
+++ public/app.js
@@ -21,6 +21,42 @@ $(document).ready(function() {
     $('.form-div').append(template);
   });
 
+
+  $('.new-artist').click(function(event) {
+    // Get New Artist Parameters
+    var name        = $('#input-name').val();
+    var picture     = $('#input-picture').val();
+    var nationality = $('#input-nationality').val();
+    var birthYear   = $('#input-birthYear').val();
+    var description = $('#input-description').val();
+    var created_at  = Date.now;
+
+    // Create New Artist Based On Params
+    $.ajax({
+      type: "POST",
+      url:  "/artists",
+      data: {
+        name:        name,
+        img_url:     picture,
+        nationality: nationality,
+        birthYear:   birthYear,
+        description: description,
+        created_at:  created_at,
+        // PAINTINGS?
+      }
+    }).done(function(createdArtist) {
+      console.log(createdArtist);
+    });
+  });
+
+  $('.new-painting').click(function(event) {
+    $('.response').empty();
+    $('.form-div').empty();
+    var source = $("#painting-form-template").html();
+    var template = Handlebars.compile(source)
+    $('.form-div').append(template);
+  });
+
 });
 
 
